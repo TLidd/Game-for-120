a9792a2c8550d206801674c1bc4bc44b43d2b38e
Tyler Liddicoat <tliddico@ucsc.edu>
2018-05-08 20:30:47 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+var game = new Phaser.Game(800, 400, Phaser.AUTO);
+
+   function preload() {
+
+   }
+
+   function create() {
+
+   }
+
+   function update() {
+
+   }
+
+	game.state.add('mainMenu', mainMenu);
+	game.state.start('mainMenu');
---
+++ menu.js
@@ -0,0 +1,17 @@
+/*
+Main menu for game prototype build
+5/8/2018
+*/
+
+var mainMenu = function(game){};
+mainMenu.prototype = {
+	preload: function(){
+		
+	},
+	create: function(){
+		game.stage.backgroundColor = "#4488AA";
+	},
+	update: function(){
+		
+	}
+}
