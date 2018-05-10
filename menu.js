1cd8eda7b58fe1d4dc3e1805b4e68a85e73ffea6
Tyler Liddicoat <tliddico@ucsc.edu>
2018-05-09 17:46:53 -0700

    setting up links

--- menu.js
+++ menu.js
@@ -10,6 +10,8 @@ mainMenu.prototype = {
 	},
 	create: function(){
 		game.stage.backgroundColor = "#4488AA";
+		mainMenuText = game.add.text(game.world.centerX, game.world.centerY, 'Main Menu');
+		mainMenuText.anchor.set(0.5, 0.5);
 	},
 	update: function(){
 		
